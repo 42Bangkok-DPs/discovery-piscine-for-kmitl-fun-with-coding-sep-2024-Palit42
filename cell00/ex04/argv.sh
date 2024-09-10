0ecb7672bcf07c75adfc90ae6f0808e621beba74
Palit42 <66125020@kmitl.ac.th>
2024-09-10 13:58:54 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,10 +1,9 @@
-if [ $# -eq 0 ]
-then
-	echo "No arguments supplied";
-	exit;
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for arg in "$1" "$2" "$3"; do
+        if [ -n "$arg" ]; then
+            echo "$arg"
+        fi
+    done
 fi
-
-for i in "$@";
-do
-	echo $i;
-done
